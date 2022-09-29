05af56abc8ffe61893f0bf5570ab32f0aefed032
Haider Ali <malikhaider0567@gmail.com>
2022-09-29 14:54:47 +0530

    added ads.txt file

--- pages/_app.js
+++ pages/_app.js
@@ -64,6 +64,11 @@ function MyApp({ Component, pageProps, router }) {
 			>
 				<Head>
 					<script
+						async
+						src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6172950963478417"
+						crossOrigin="anonymous"
+					></script>
+					<script
 						src="https://accounts.google.com/gsi/client"
 						async
 						defer
