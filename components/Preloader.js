c370fc1fffcd5eb2c0eb7b01ca3d79d83de1c63e
FireeagSaini <84465137+FireeagSaini@users.noreply.github.com>
2022-10-21 22:41:54 +0530

    add prettier config file (#33)
    * add prettier config file
    
    * Update .prettierignore
    
    added yarn.lock
    
    Co-authored-by: rohit.saini1 <rohit.saini@valyu.ai>
    Co-authored-by: Anmol Agrawal <88332977+anmode@users.noreply.github.com>

--- components/Preloader.js
+++ components/Preloader.js
@@ -1,14 +1,14 @@
-import styles from '@styles/scss/preloader.module.scss';
+import styles from "@styles/scss/preloader.module.scss";
 
 const Preloader = () => {
-	return (
-		<div className={styles.ldsEllipsis}>
-			<div></div>
-			<div></div>
-			<div></div>
-			<div></div>
-		</div>
-	);
+  return (
+    <div className={styles.ldsEllipsis}>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
 };
 
 export default Preloader;
--- components/Team.js
+++ components/Team.js
@@ -1,77 +1,77 @@
-import styles from '@styles/scss/main.module.scss';
-import Card from './Teamcard';
+import styles from "@styles/scss/main.module.scss";
+import Card from "./Teamcard";
 
 const Team = () => {
-	return (
-		<>
-			<h2 className={styles.heading}>Our Team</h2>
-			<div className={styles.team}>
-				<Card
-					facebook={'#'}
-					insta={'#'}
-					twitter={'#'}
-					linkedin={'#'}
-					name={'Haider Ali'}
-					photo={'/images/team/haider.jpg'}
-					desig={'Co-Founder and Core Developer'}
-				/>
-				<Card
-					facebook={'#'}
-					insta={'#'}
-					twitter={'#'}
-					linkedin={'#'}
-					name={'Name Here'}
-					photo={''}
-					desig={'Designation'}
-				/>
-				<Card
-					facebook={'#'}
-					insta={'#'}
-					twitter={'#'}
-					linkedin={'#'}
-					name={'Name Here'}
-					photo={''}
-					desig={'Designation'}
-				/>
-				<Card
-					facebook={'#'}
-					insta={'#'}
-					twitter={'#'}
-					linkedin={'#'}
-					name={'Name Here'}
-					photo={''}
-					desig={'Designation'}
-				/>
-				<Card
-					facebook={'#'}
-					insta={'#'}
-					twitter={'#'}
-					linkedin={'#'}
-					name={'Name Here'}
-					photo={''}
-					desig={'Designation'}
-				/>
-				<Card
-					facebook={'#'}
-					insta={'#'}
-					twitter={'#'}
-					linkedin={'#'}
-					name={'Name Here'}
-					photo={''}
-					desig={'Designation'}
-				/>
-				<Card
-					facebook={'#'}
-					insta={'#'}
-					twitter={'#'}
-					linkedin={'#'}
-					name={'Name Here'}
-					photo={''}
-					desig={'Designation'}
-				/>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <h2 className={styles.heading}>Our Team</h2>
+      <div className={styles.team}>
+        <Card
+          facebook={"#"}
+          insta={"#"}
+          twitter={"#"}
+          linkedin={"#"}
+          name={"Haider Ali"}
+          photo={"/images/team/haider.jpg"}
+          desig={"Co-Founder and Core Developer"}
+        />
+        <Card
+          facebook={"#"}
+          insta={"#"}
+          twitter={"#"}
+          linkedin={"#"}
+          name={"Name Here"}
+          photo={""}
+          desig={"Designation"}
+        />
+        <Card
+          facebook={"#"}
+          insta={"#"}
+          twitter={"#"}
+          linkedin={"#"}
+          name={"Name Here"}
+          photo={""}
+          desig={"Designation"}
+        />
+        <Card
+          facebook={"#"}
+          insta={"#"}
+          twitter={"#"}
+          linkedin={"#"}
+          name={"Name Here"}
+          photo={""}
+          desig={"Designation"}
+        />
+        <Card
+          facebook={"#"}
+          insta={"#"}
+          twitter={"#"}
+          linkedin={"#"}
+          name={"Name Here"}
+          photo={""}
+          desig={"Designation"}
+        />
+        <Card
+          facebook={"#"}
+          insta={"#"}
+          twitter={"#"}
+          linkedin={"#"}
+          name={"Name Here"}
+          photo={""}
+          desig={"Designation"}
+        />
+        <Card
+          facebook={"#"}
+          insta={"#"}
+          twitter={"#"}
+          linkedin={"#"}
+          name={"Name Here"}
+          photo={""}
+          desig={"Designation"}
+        />
+      </div>
+    </>
+  );
 };
 
 export default Team;
