5cd5220c5cbcedd3734c7c5d45552964de2eb04e
Haider Ali <malikhaider0567@gmail.com>
2022-10-26 14:07:46 +0530

    Opportunity Details Page Updated

--- pages/opportunities/OppDetailsId.js
+++ pages/opportunities/OppDetailsId.js
@@ -1,9 +1,12 @@
-import { useRouter } from 'next/router';
-import OppDetails from 'components/OppDetails';
+import { useRouter } from "next/router";
+import OppDetails from "components/OppDetails";
 
-export default function BlogId() {
-
-	const router = useRouter();
-	const { oppId } = router.query;
-	return <div><OppDetails></OppDetails></div>;
+export default function OppDetailsId() {
+  const router = useRouter();
+  const { oppId } = router.query;
+  return (
+    <div>
+      <OppDetails></OppDetails>
+    </div>
+  );
 }
