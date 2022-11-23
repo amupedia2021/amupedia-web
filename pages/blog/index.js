1f22b34c046d75c821ad911b57234a0480e11260
Sayyid Muhammad A <97773130+SayyidMuhammad878@users.noreply.github.com>
2022-11-23 14:39:31 +0700

    Create the backend of blogs page and integrated to the frontend (#48)
    * Create opportunities card backend
    
    * Improve the opportunities backend
    
    * Create the backend of the blogs page and integrated it with the frontend

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -3,9 +3,18 @@ import BlogCard from "components/BlogCard";
 import Footer from "components/Footer";
 import Header from "components/Header";
 import styles from "@styles/scss/blog.module.scss";
-import blogData from "/data/blogdata";
+// import blogData from "/data/blogdata";
 
-export default function Blogs() {
+export const getStaticProps = async () => {
+	const res = await fetch('http://localhost:3000/api/blogs/fetchBlogs');
+	const blogsData = await res.json();
+
+	return {
+		props: { blogsData }
+	}
+}
+
+export default function Blogs({ blogsData }) {
 	return (
 		<>
 			<Head>
@@ -15,13 +24,14 @@ export default function Blogs() {
 			<Header image="/images/blog/blog_bg.svg" text="Blogs" />
 			{/* <Link href="/blog/writeblog">Write a blog</Link> */}
 			<div className={styles.container}>
-				{blogData.map((blogItem) => (
+				{blogsData.result.map((blogItem, index) => (
 					<BlogCard
-						key={blogItem.id}
-						id={blogItem.id}
-						image={blogItem.image}
-						comments={blogItem.comments}
-						likes={blogItem.likes}
+						key={blogItem.userId}
+						id={index}
+						title={blogItem.title}
+						image={blogItem.coverImg}
+						comments={blogItem.numberOfComments}
+						likes={blogItem.like}
 					/>
 				))}
 			</div>
