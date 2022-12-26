280aa52c00fd12d884d50fa54c6484ca06813199
Haider Ali <malikhaider0567@gmail.com>
2022-12-26 11:47:36 +0530

    fixed lint errors

--- components/Blogs/WriteBlogs/WriteBlogLayoutStyles.js
+++ components/Blogs/WriteBlogs/WriteBlogLayoutStyles.js
@@ -6,28 +6,28 @@ export const Wrapper = styled.section`
   display: flex;
   height: 100vh;
   width: 100vw;
-`
+`;
 
 export const Sidebar = styled.div`
   background-color: black;
   height: 100%;
   min-width: 250px;
-`
+`;
 
 export const Main = styled.main`
   padding: 20px;
   display: flex;
   flex-direction: column;
-`
+`;
 export const TitleWrapper = styled.div`
   margin: 15px 0;
   position: relative;
-`
+`;
 export const BlogTitle = styled(ContentEditable)`
   text-align: left;
-  outline : none;
+  outline: none;
   margin: 0;
-`
+`;
 export const TitlePlaceholder = styled.h2`
   color: #a8a8a8;
   margin: 0;
@@ -36,7 +36,7 @@ export const TitlePlaceholder = styled.h2`
   height: 100%;
   inset: 0;
   z-index: -1;
-`
+`;
 
 export const BlogContent = styled.div`
   overflow: auto;
@@ -45,4 +45,4 @@ export const BlogContent = styled.div`
   padding: 10px 20px;
   border-radius: 15px;
   font-size: 22px;
-`
+`;
