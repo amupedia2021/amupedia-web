02d2032ced6371c4bb7b79d2807779746890b82b
swarnava <swarnava.seal@aegon.co.uk>
2023-05-28 23:52:56 +0100

    warning removed

--- components/Curriculum/Exams.js
+++ components/Curriculum/Exams.js
@@ -6,11 +6,9 @@ const Exams = ({ data }) => {
                 data.map((unit, index) => {
                     const title = unit.title;
                     return (
-                        <>
-                            <div key={index} >
-                                <p> {title} </p>
-                            </div>
-                        </>
+                        <div key={index}>
+                            <p> {title} </p>
+                        </div>
                     )
                 })
             }
