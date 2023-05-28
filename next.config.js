4ad4d70ed461ec4addb43f4bd6448a2950898484
Aman Jain <89217032+AmanJain18@users.noreply.github.com>
2023-05-28 09:48:28 +0530

    Revert "added coming soon page (#96)"
    This reverts commit c4280b938d52260c7b405e756d2926e5988d6156.

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 const withImages = require("next-images");
 module.exports = withImages();
-module.exports = withImages({
-  webpack(config, options) {
-    return config
-  }
-});
+module.exports = {
+  reactStrictMode: true,
+};
 
 const path = require("path");
 
