bcb5cb6dca13e7d764778a1d4b85599d22a5fc2c
Seal <Swarnava.Seal@aegon.co.uk>
2023-06-01 16:08:55 +0100

    warning removed

--- components/Curriculum/Exams.js
+++ components/Curriculum/Exams.js
@@ -1,15 +1,14 @@
 import styles from '@styles/Home.module.css';
-import Link from 'next/Link';
-import Tilt from 'react-vanilla-tilt';
+import Link from 'next/link';
 
 const Exams = ({ data }) => {
     return (
         <>
             {
-                data.map((unit) => {
+                data.map((unit, index) => {
                     return (
                         <>
-                            <div className={styles.grabnotes}>
+                            <div className={styles.grabnotes} key={index}>
                                 <Link passHref href='/'>
                                     <div style={{
                                         background: `url(${unit.icon}) no-repeat center center`,
