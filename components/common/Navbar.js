86426c1ffd3e2e49c6fc9bd4383143df3ef10572
swarnava <swarnava.seal@aegon.co.uk>
2023-06-09 22:19:53 +0100

    conflict resolve

--- components/common/Navbar.js
+++ components/common/Navbar.js
@@ -6,9 +6,9 @@ import { useState, useEffect } from 'react';
 const Navbar = () => {
   const router = useRouter();
   const [navbar, setNavbar] = useState(false);
-  const [hideNavBar, setHideNavBar] = useState(false);
   const [courseActive, setCourseActive] = useState(false);
   const [contactActive, setContactActive] = useState(false);
+  const [dropdown, setDropdown] = useState(false); //for adding dropdown feature
 
   useEffect(() => {
     window.addEventListener('scroll', changeNavbar);
@@ -48,27 +48,45 @@ const Navbar = () => {
     obs.observe(contacts);
   }, []);
 
+  // const changeNavbar = () => {
+  //   if (window.scrollY > 500) {
+  //     setNavbar(true);
+  //   } else setNavbar(false);
+  // };
   const changeNavbar = () => {
-    if (window.scrollY > 300) {
-      setNavbar(true);
-      setHideNavBar(false);
-    }
-    else if (window.scrollY > 150) {
-      setHideNavBar(true);
-    }
-    else {
-      setNavbar(false);
-      setHideNavBar(false);
+    const courses = document.getElementById('courses');
+    if (courses) {
+      if (window.scrollY > 500) {
+        setNavbar(true);
+        setCourseActive(true);
+      } else {
+        setNavbar(false);
+        setCourseActive(false);
+      }
+    } else {
+      setNavbar(window.scrollY > 500);
     }
   };
 
-  const onClick = () => {
-    // Handles mobile course click
-    const menu = document.querySelector('#menu');
-    if (menu.checked) menu.click();
+  
+
+  const handleCoursesHover = () => {
+    if (window.innerWidth >= 768) {
+      // Show the dropdown menu only on larger screens
+      setDropdown(true);
+    }
   };
 
-  return (!hideNavBar &&
+  const handleCoursesClick = () => {
+    if (window.innerWidth < 768) {
+      // Show/hide the dropdown menu on small screens
+      setDropdown(!dropdown);
+      
+    }
+  };
+
+
+  return (
     <>
       {/* <!-- hamburger menu icon (for mobile phones) --> */}
       <input type='checkbox' className={styles.menu} id='menu' />
@@ -79,7 +97,17 @@ const Navbar = () => {
       </label>
 
       {/* <!-- overlay to be shown when nav bar becomes visible --> */}
-      <div className={styles.blackoverlay} id='overlay' onClick={onClick}></div>
+      {/* <div className={styles.blackoverlay} id='overlay' ></div> */}
+      <div
+        className={`${styles.blackoverlay} ${dropdown ? styles.overlay_active : ''}`}
+        id='overlay'
+        onClick={() => {
+                  const menu = document.querySelector('#menu');
+                  if (menu.checked) menu.click();
+                  setDropdown(false);
+                }}
+        
+      ></div>
 
       {/* <!-- navigation bar --> */}
       <nav
@@ -96,14 +124,19 @@ const Navbar = () => {
               router.pathname === '/' && !courseActive && !contactActive && navbar
                 ? styles.navList_active
                 : ''
-              }`}
+            }`}
           >
             <Link href='/'>
               <a
-                onClick={onClick}
+                // onClick={onClick}: replace this with below to ensure that the menu is properly closed when the element is clicked.
+                onClick={() => {
+                  const menu = document.querySelector('#menu');
+                  if (menu.checked) menu.click();
+                  
+                }}
                 className={`${styles.lnk} ${
                   router.pathname === '/' && !courseActive && !contactActive ? styles.active : ''
-                  } ${navbar && styles.nav_active}`}
+                } ${navbar && styles.nav_active}`}
               >
                 Home
               </a>
@@ -112,40 +145,81 @@ const Navbar = () => {
           <li
             className={`${styles.listitem} ${
               courseActive && navbar ? styles.navList_active : ''
-              }`}
-          >
-            <Link href='/'>
-              <a
-                onClick={() => {
-                  router.push('/');
-                  setTimeout(() => {
-                    const courses = document.getElementById('courses');
-                    if (courses) courses.scrollIntoView();
+            }`}
 
-                    onClick();
-                  }, 10);
-                }}
-                className={`${styles.lnk} ${
-                  courseActive ? styles.active : ''
-                  } ${navbar && styles.nav_active}`}
+            
+            onMouseEnter={handleCoursesHover} // Add mouse enter event for larger screens
+            onClick={handleCoursesClick} // Add click event for smaller screens
+
+          >
+              <a
+              onClick={() => {
+                router.push('/');
+                setTimeout(() => {
+                  const courses = document.getElementById('courses');
+                  if (courses) courses.scrollIntoView();
+                  //onClick();
+                }, 10);
+              }}
+              
+              className={`${styles.lnk} ${courseActive ? styles.active : ''} ${navbar && styles.nav_active}`}
               >
-                Courses
+                Courses 
               </a>
-            </Link>
+            
+            {/* Dropdown menu for courses */}
+            {dropdown && (
+              <ul className={styles.dropdownMenu} onMouseLeave={() => setDropdown(false)}>
+               <li>
+                 <Link href='/courses/btech'>
+                   <a onClick={() => setDropdown(false)} className={styles.dropdownLink}>
+                     B.Tech
+                   </a>
+                 </Link>
+                </li>
+                <li>
+                 <Link href='/'>
+                   <a onClick={() => setDropdown(false)} className={styles.dropdownLink}>
+                    BE
+                   </a>
+                 </Link>
+                </li>
+                <li>
+                 <Link href='/'>
+                   <a onClick={() => setDropdown(false)} className={styles.dropdownLink}>
+                    B.Com
+                   </a>
+                 </Link>
+                </li>
+                <li>
+                 <Link href='/'>
+                   <a onClick={() => setDropdown(false)} className={styles.dropdownLink}>
+                    BSC
+                   </a>
+                 </Link>
+                </li>
+                
+                {/* Add more courses as needed */}
+              </ul>
+            )}
           </li>
           <li
             className={`${styles.listitem} ${
               router.pathname === '/about' && navbar
                 ? styles.navList_active
                 : ''
-              }`}
+            }`}
           >
             <Link href='/about'>
               <a
-                onClick={onClick}
+               
+                onClick={() => {
+                  const menu = document.querySelector('#menu');
+                  if (menu.checked) menu.click();
+                }}
                 className={`${styles.lnk} ${
                   router.pathname === '/about' ? styles.active : ''
-                  } ${navbar && styles.nav_active}`}
+                } ${navbar && styles.nav_active}`}
               >
                 About
               </a>
@@ -171,14 +245,17 @@ const Navbar = () => {
           <li
             className={`${styles.blg} ${styles.listitem} ${
               router.pathname === '/blog' && navbar ? styles.navList_active : ''
-              }`}
+            }`}
           >
             <Link href='/blog'>
               <a
-                onClick={onClick}
+                onClick={() => {
+                  const menu = document.querySelector('#menu');
+                  if (menu.checked) menu.click();
+                }}
                 className={`${styles.lnk} ${
                   router.pathname === '/blog' ? styles.active : ''
-                  } ${navbar && styles.nav_active}`}
+                } ${navbar && styles.nav_active}`}
               >
                 Blogs
               </a>
@@ -190,14 +267,17 @@ const Navbar = () => {
               router.pathname === '/contributors' && navbar
                 ? styles.navList_active
                 : ''
-              }`}
+            }`}
           >
             <Link href='/contributors'>
               <a
-                onClick={onClick}
+                onClick={() => {
+                  const menu = document.querySelector('#menu');
+                  if (menu.checked) menu.click();
+                }}
                 className={`${styles.lnk} ${
                   router.pathname === '/contributors' ? styles.active : ''
-                  } ${navbar && styles.nav_active}`}
+                } ${navbar && styles.nav_active}`}
               >
                 Contributors
               </a>
@@ -206,7 +286,7 @@ const Navbar = () => {
           <li
             className={`${styles.listitem} ${
               contactActive && navbar ? styles.navList_active : ''
-              }`}
+            }`}
           >
             <a
               onClick={() => {
@@ -214,12 +294,12 @@ const Navbar = () => {
                 setTimeout(() => {
                   const contacts = document.getElementById('contacts');
                   if (contacts) contacts.scrollIntoView();
-                  onClick();
+                  //onClick();
                 }, 10);
               }}
               className={`${styles.lnk} ${contactActive ? styles.active : ''} ${
                 navbar && styles.nav_active
-                }`}
+              }`}
             >
               Contact Us
             </a>
