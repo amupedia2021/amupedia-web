e9c2d8082f6ed1243c60bf06829a8e4b744e1bc4
Vaibhav Maurya <vibhu.astute@gmail.com>
2023-07-13 15:33:30 +0530

    fix search issue

--- pages/contributors/index.js
+++ pages/contributors/index.js
@@ -4,28 +4,27 @@ import ContCard from 'components/Contributors/ContCard';
 import Footer from 'components/common/Footer';
 import Header from 'components/common/Header/Header';
 import styles from '@styles/scss/contributor.module.scss';
-import { each } from 'lodash';
 
 const Contributors = () => {
-  const [contributors, setContributors] = useState([]);
+  const per_page = 10;
   const [currentPage, setCurrentPage] = useState(1);
+  const [allContributors, setAllContributors] = useState([]);
+  const [contributors, setContributors] = useState([]);
   const [totalPages, setTotalPages] = useState(0);
-  const [username, setUsername] = useState("")
+  const [username, setUsername] = useState('');
 
-  const fetchContributors = async (page) => {
-    const url = `https://api.github.com/repos/amupedia2021/Project-Amupedia/contributors?page=${page}&per_page=10`;
+  const fetchContributors = async () => {
     try {
-      const response = await fetch(url);
+      const response = await fetch(
+        'https://api.github.com/repos/amupedia2021/Project-Amupedia/contributors?per_page=100'
+      );
 
       if (response.ok) {
         const contributorsData = await response.json();
         const contributorsDataFiltered = contributorsData.filter(
           (contributor) => !contributor.login.includes('dependabot[bot]')
         );
-        setContributors(contributorsDataFiltered);
-        const linkHeader = response.headers.get('Link');
-        const totalPages = extractTotalPages(linkHeader);
-        setTotalPages(totalPages);
+        setAllContributors(contributorsDataFiltered);
       } else {
         console.error('Failed to fetch contributors:', response.status);
       }
@@ -34,32 +33,38 @@ const Contributors = () => {
     }
   };
 
-  const extractTotalPages = (linkHeader) => {
-    if (!linkHeader) return 0;
-    const regex = /<.+[?&]page=(\d+)&per_page=\d+>; rel="last"/;
-    const matches = linkHeader.match(regex);
-    return matches ? parseInt(matches[1]) : 0;
-  };
-
   const handleNextPage = () => {
     if (currentPage < totalPages) {
-      const nextPage = currentPage + 1;
-      fetchContributors(nextPage);
-      setCurrentPage(nextPage);
+      setCurrentPage(currentPage + 1);
     }
   };
 
   const handlePreviousPage = () => {
     if (currentPage > 1) {
-      const previousPage = currentPage - 1;
-      fetchContributors(previousPage);
-      setCurrentPage(previousPage);
+      setCurrentPage(currentPage - 1);
     }
   };
 
   useEffect(() => {
-    fetchContributors(currentPage);
-  }, [currentPage]);
+    fetchContributors();
+  }, []);
+
+  useEffect(() => {
+    const filteredContributors = allContributors.filter((eachContributor) =>
+      eachContributor.login.toLowerCase().includes(username.toLowerCase())
+    );
+    setContributors(filteredContributors);
+
+    const totalPage = Math.ceil(filteredContributors.length / per_page);
+    setTotalPages(totalPage);
+
+    setCurrentPage(1)
+  }, [username, allContributors]);
+
+  const visibleContributors = contributors.slice(
+    (currentPage - 1) * per_page,
+    currentPage * per_page
+  );
 
   return (
     <>
@@ -72,10 +77,16 @@ const Contributors = () => {
       </Head>
       <Header text='Contributors' />
       <div className={styles.container}>
-        <input type="text" className={styles.details} placeholder='enter github username' value={username} onChange={e => setUsername(e.target.value)}/>
+        <input
+          type='text'
+          className={styles.details}
+          placeholder='Enter GitHub username'
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
       </div>
       <div className={styles.container}>
-        {contributors.filter((eachContributor) => eachContributor.login.toLowerCase().includes(username.toLowerCase())).map((contributor) => (
+        {visibleContributors.map((contributor) => (
           <ContCard
             key={contributor.id}
             image={contributor.avatar_url}
@@ -89,7 +100,7 @@ const Contributors = () => {
         <button onClick={handlePreviousPage} disabled={currentPage === 1}>
           Prev
         </button>
-        <span>{currentPage}</span>
+        <span>{currentPage} / {totalPages}</span>
         <button onClick={handleNextPage} disabled={currentPage === totalPages}>
           Next
         </button>
