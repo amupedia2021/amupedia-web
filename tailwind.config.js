9ba65d515e579dcb2f5a8619f6c7bfa68e8eb839
akanksha984 <97845909+akanksha984@users.noreply.github.com>
2023-07-14 22:21:29 +0530

    Update tailwind.config.js
    Signed-off-by: akanksha984 <97845909+akanksha984@users.noreply.github.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./data/**/*.{js,ts,jsx,tsx,mdx}",
  
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
