8c6e65227218b8ab88ddc67c9a3c66e71b789931
Nishitbariya <85815172+Nishitbaria@users.noreply.github.com>
2023-07-22 22:04:50 +0530

    Added SearchResult NotFound in Blogs and Contributors

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -7,11 +7,18 @@ import blogData from "/data/blogdata";
 import GoToTop from "components/GoToTop";
 import WriteBlog from "./writeblog";
 import { useState } from "react";
+import SearchResultNotFound from "../../components/common/SearchResultNotFound";
+
 export default function Blogs() {
 	const [query, setQuery] = useState("");
 	const handleSearch = (e) => {
 		setQuery(e.target.value)
 	}
+	const filteredBlogData = blogData.filter(
+		(blogItem) =>
+		  blogItem.title.toLowerCase().includes(query.toLowerCase())
+	  );
+	
 	return (
 		<>
 			<GoToTop />
@@ -33,21 +40,22 @@ export default function Blogs() {
 			</div>
 			{/* <Link href="/blog/writeblog">Write a blog</Link> */}
 			<div className={styles.container}>
-				{blogData.filter(
-					(blogItem) => blogItem.title
-						.toLowerCase().includes(query.toLowerCase()))
-					.map((blogItem) => (
-						<BlogCard
-							author={blogItem.author}
-							title={blogItem.title}
-							key={blogItem.id}
-							id={blogItem.id}
-							image={blogItem.image}
-							comments={blogItem.comments}
-							likes={blogItem.likes}
-						/>
-					))}
-			</div>
+        {filteredBlogData.length > 0 ? (
+          filteredBlogData.map((blogItem) => (
+            <BlogCard
+              author={blogItem.author}
+              title={blogItem.title}
+              key={blogItem.id}
+              id={blogItem.id}
+              image={blogItem.image}
+              comments={blogItem.comments}
+              likes={blogItem.likes}
+            />
+          ))
+        ) : (
+          <SearchResultNotFound query={query} />
+        )}
+      </div>
 			<Footer />
 		</>
 	);
