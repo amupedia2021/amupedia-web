ec2d2b765ef60525b6b371640cac6d66e49d0dc2
Nishitbariya <85815172+Nishitbaria@users.noreply.github.com>
2023-08-07 20:10:35 +0530

    Added Competitive Exam Section with Tables, Images, and Exam Details (#590)
    * Added SearchResult NotFound in Blogs and Contributors
    
    * style : fix color of humburger ✨
    
    * Added Relevant content to competitive exam section with tables and images
    
    * Fixed Image
    
    * Revert "Fixed Image"
    
    This reverts commit bea3c4140e7d0a260af6c22e5e7cfd2c6df357bd.

--- components/Curriculum/ExamCard.js
+++ components/Curriculum/ExamCard.js
@@ -1,21 +1,55 @@
-import styles from '@styles/scss/exam.module.scss';
 import Image from 'next/image';
+import styles from '@styles/scss/exam.module.scss'; // Import the SCSS file
 
 const ExamCard = ({ data }) => {
-    return (
-        <>
-            <div className={styles.material}>
-                <div className={styles.imageDiv}>
-                    <Image loading="lazy" src={`${data.icon}`} width={0} height={0} sizes="100vh"></Image>
-                </div>
-                <div className={styles.detailDiv}>
-                    <p>DATE :  {data.date}</p>
-                    <p>MIN QUALIFICATION :  {data.minQualification}</p>
-                    <p>BENEFITS :  {data.benefit}</p>
-                </div>
-            </div>
-        </>
-    );
+ 
+
+  return (
+    <>
+    <div className={styles.material}>
+      {/* Place the image div after the table div to ensure it comes on top */}
+      <div className={styles.imageDiv}>
+        <Image
+          loading="lazy"
+          src={`${data.icon}`}
+          width={464}
+          height={464}
+          layout="responsive"
+          alt="Exam Icon"
+        />
+      </div>
+      <div className="ml-9 mb-8">
+        <table
+          className="border-collapse rounded-lg overflow-hidden w-120 "
+          style={{       fontSize: '16px', lineHeight: '1.6', background: '#f5f5f5' }}
+        >
+          <tbody>
+            {data.examDetails.map((detail, index) => (
+              <tr
+                key={index}
+                className="hover:bg-yellow-200 transition-all duration-200"
+              >
+                <td
+                  style={{ width: '300px' }}
+                  className="p-2 border border-black"
+                >
+                  <p className="text-purple-700 font-bold">{detail.title}</p>
+                </td>
+                <td
+                  style={{ width: '301px' }}
+                  className="p-2 border border-black"
+                >
+                  <p className="text-blue-700">{detail.detail}</p>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+     
+    </div>
+  </>
+);
 };
 
 export default ExamCard;
--- data/examData.js
+++ data/examData.js
@@ -2,7 +2,21 @@ export const competitiveExams = [{
     title: "GATE",
     desc: "Information about entry-level examinations and examinations held as needed in particular occupational groups in the General Service and related categories.",
     date: "15/04/2024",
-    icon: "/images/exam/gate.svg",
+    icon: "/images/exam/download.png",
+    examDetails : [
+        { title: 'Entrance Examination', detail: 'Graduate Aptitude Test in Engineering (GATE)' },
+        { title: 'Examination Conducting Body', detail: '7 Indian Institutes of Technology (IITs), and Indian Institute of Science Bangalore (IISc Bangalore)' },
+        { title: 'Official Website', detail: 'gate.iitk.ac.in for GATE 2023' },
+        { title: 'Examination Level', detail: 'National Level' },
+        { title: 'Examination Type', detail: 'Computer Based Test' },
+        { title: 'Application Mode', detail: 'Online' },
+        { title: 'Type of Questions', detail: 'Multiple Choice Questions and Numerical-based Questions' },
+        { title: 'Periodicity', detail: 'Yearly' },
+        { title: 'Time Duration', detail: '3 Hours i.e. 180 minutes' },
+        { title: 'Score Range', detail: '1000' },
+        { title: 'No. of Test Takers', detail: '~ 9,00,000 every year' },
+        { title: 'Validity of GATE Score', detail: '3 years' },
+      ],
     minQualification: "10+2",
     benefit: "This will get you M Tech"
 },
@@ -10,7 +24,23 @@ export const competitiveExams = [{
     title: "SSC",
     desc: "Examinations for recruitment at the P-1 and P-2 levels.",
     date: "25/04/2024",
-    icon: "/images/exam/ssc.svg",
+    icon: "/images/exam/images.jpg",
+    examDetails : [
+        { title: 'Conducting Body', detail: 'Staff Selection Commission (SSC)' },
+        { title: 'Exams Level', detail: 'National' },
+        { title: 'Exams Purpose', detail: 'Selection of candidates in Group B and C posts in ministries/departments/organisations of the Government of India' },
+        { title: 'Exams Frequency', detail: 'Once a year' },
+        { title: 'Exams Language', detail: 'English and Hindi' },
+        { title: 'Official Website', detail: 'https://ssc.nic.in/' },
+        { title: 'SSC CGL 2023', detail: 'Assistant Audit Officer, Assistant Accounts Officer, Assistant Section Officer, Inspector of Income Tax, Assistant Superintendent, Divisional Accountant, Junior Statistical Officer, Auditor, Tax Assistant, Upper Divisions Clerk, etc.' },
+        { title: 'SSC CHSL 2023', detail: 'Lower Divisional Clerk (LDC), Junior Secretariat Assistant (JSA), Postal Assistant (PA), Sorting Assistant (SA), Data Entry Operator (DEO) and DEO (Grade A)' },
+        { title: 'SSC JE 2023', detail: 'Civil Junior Engineer, Electrical Junior Engineer, Mechanical Junior Engineer, Surveying and Contract Junior Engineer' },
+        { title: 'SSC GD 2022-23', detail: 'Constable (General Duty)' },
+        { title: 'SSC CPO 2023', detail: 'Sub-Inspector and Assistant Sub-Inspector' },
+        { title: 'SSC JHT 2023', detail: 'Junior Translator, Senior Hindi Translator, Junior Hindi Translator and Hindi Pradhyapak' },
+        { title: 'SSC Stenographer 2023', detail: 'Stenographer Grade-C (Group B-Non-Gazetted) and Stenographer Grade-D (Group-C)' },
+        { title: 'SSC MTS 2023', detail: 'Multi-tasking staff' },
+      ],
     minQualification: "Graduation",
     benefit: "This will get you in school service"
 },
@@ -18,7 +48,29 @@ export const competitiveExams = [{
     title: "MAT",
     desc: "Examinations for recruitment at the P-1 and P-2 levels.",
     date: "10/04/2024",
-    icon: "/images/exam/mat.svg",
+    examDetails : [
+        { title: 'Conducting Body', detail: 'All India Management Association (AIMA)' },
+        { title: 'Exams Level', detail: 'National' },
+        { title: 'Exams Purpose', detail: 'Admission to MBA and allied programs in various participating institutes' },
+        { title: 'Exams Frequency', detail: 'Multiple times a year (Usually conducted in February, May, September, and December)' },
+        { title: 'Exams Language', detail: 'English' },
+        { title: 'Official Website', detail: 'https://www.aima.in/' },
+        { title: 'Mode of Examination', detail: 'Both Paper-based Test (PBT) and Computer-based Test (CBT)' },
+        { title: 'Number of Sections', detail: '5 Sections: Language Comprehension, Mathematical Skills, Data Analysis and Sufficiency, Intelligence and Critical Reasoning, Indian and Global Environment' },
+        { title: 'Total Questions', detail: '200' },
+        { title: 'Total Marks', detail: '200' },
+        { title: 'Marking Scheme', detail: 'Each correct answer carries 1 mark. There is a negative marking of 0.25 marks for each wrong answer.' },
+        { title: 'Exam Duration', detail: '150 minutes (2 hours 30 minutes)' },
+        { title: 'MAT Score Validity', detail: 'Generally, the MAT score is valid for one year.' },
+      ],
+     
+      
+      
+      
+      
+      
+      
+    icon: "/images/exam/matfinals.jpg",
     minQualification: "10+2",
     benefit: "This will get you into MBA"
 },
@@ -26,7 +78,25 @@ export const competitiveExams = [{
     title: "CAT",
     desc: "Information about entry-level examinations and examinations held as needed in particular occupational groups in the General Service and related categories.",
     date: "15/05/2024",
-    icon: "/images/exam/cat.svg",
+    examDetails:[    { title: 'Particular', detail: 'Detail' },
+    { title: 'CAT Full Form', detail: 'Common Admission Test (CAT)' },
+    { title: 'CAT Exam Date 2023', detail: '26 November 2023' },
+    { title: 'Examination Type', detail: 'National Level Examination' },
+    { title: 'CAT Exam Sessions', detail: '3 sessions – Morning, Afternoon, and Evening' },
+    { title: 'CAT 2023 Exam Conducting Body', detail: 'IIM Lucknow' },
+    { title: 'CAT Exam Mode', detail: 'Computer Based (but not Online)' },
+    { title: 'CAT Age Limit', detail: 'No age limit\nGraduates with 50% marks in aggregate can apply' },
+    { title: 'No. of Questions in CAT Exam 2023', detail: 'Expected 64-68 questions divided among 3 sections' },
+    { title: 'CAT Negative Marking', detail: '1 mark will be deducted for each wrong answer of MCQs.\nNo mark would be deducted for Non-MCQs' },
+    { title: 'CAT Exam Duration', detail: '2 hours, i.e. 120 minutes (40 min for each section)' },
+    { title: 'Section wise questions', detail: 'Expected:\nVARC – 22 questions\nDILR – 20 questions\nQA – 22 questions' },
+    { title: 'CAT Total Marks', detail: '198 marks' },
+    { title: 'Type of Questions', detail: 'MCQ and TITA (Type in the Answer questions)' },
+    { title: 'Difficulty level of CAT Exam', detail: 'Moderate to Difficult' },
+    { title: 'Colleges Accepting CAT Score 2023', detail: '20 IIMs, FMS, MDI, SPJIMR, IITs, IMI, JBIMS & 500 other MBA colleges in India' },
+    { title: 'CAT Exam Fees', detail: 'INR 2400 for General Category\nINR 1200 for SC/ST/PwD' },
+    { title: 'Official Website', detail: 'http://iimcat.ac.in/' },],
+    icon: "/images/exam/catexamszz.jpg",
     minQualification: "Graduation",
     benefit: "This will get you into MBA"
 },
@@ -34,7 +104,17 @@ export const competitiveExams = [{
     title: "CUET",
     desc: " A single examination will enable the Candidates to cover a wide outreach and be part of the admissions process to various Central Universities.",
     date: "10/04/2025",
-    icon: "/images/exam/mat.svg",
+    examDetails : [
+        { title: 'Exam Name', detail: 'CUET PG 2024' },
+        { title: 'Conducting Authority', detail: 'National Testing Agency (NTA)' },
+        { title: 'Level of Exam', detail: 'National Level' },
+        { title: 'Courses Offered', detail: 'Postgraduate' },
+        { title: 'Exam Frequency', detail: 'Once a Year' },
+        { title: 'Mode of Application', detail: 'Online' },
+        { title: 'Exam Mode', detail: 'Online (Computer Based Test)' },
+        { title:'Website',detail:'https://nta.ac.in/Cuetexam'}
+      ],
+    icon: "/images/exam/cuets.jpg",
     minQualification: "Graduation",
     benefit: "The Common University Entrance Test (CUET) will provide a common platform and equal opportunities to candidates across the country"
 },
@@ -42,7 +122,33 @@ export const competitiveExams = [{
     title: "UGC",
     desc: "University Grants Commission.",
     date: "15/05/2024",
-    icon: "/images/exam/cat.svg",
+    examDetails :[
+        { title: 'Exam Name', detail: 'UGC NET (University Grants Commission National Eligibility Test)' },
+        { title: 'Conducting Body', detail: 'National Testing Agency (NTA)' },
+        { title: 'Exam Level', detail: 'National' },
+        { title: 'Exam Frequency', detail: 'Twice a year' },
+        { title: 'Mode of Exam', detail: 'Online - CBT (Computer-Based Test)' },
+        { title: 'Language/Medium of Exam', detail: 'English and Hindi' },
+        { title: 'Official Website', detail: 'https://ugcnet.nta.nic.in' },
+        { title: 'No. of Applicants', detail: '6.39 Lakh (June 2023)' },
+        { title: 'No. of Test-takers', detail: '4.63 Lakh (June 2023)' },
+        { title: 'Exam Fees', detail: 'INR 1,100 for General, INR 550 for OBC, INR 275 for SC/ST/PwD' },
+        { title: 'Exam Duration', detail: '180 minutes' },
+        { title: 'Exam Time', detail: 'Shift 1 - 09:00 am to 12:00 noon, Shift 2 - 03:00 pm to 06:00 pm' },
+        { title: 'No. of Papers and Total Marks', detail: 'Paper-1: 100 marks, Paper-2: 200 marks' },
+        { title: 'Total Questions', detail: '50 MCQs in Paper 1 and 100 MCQs in Paper 2' },
+        { title: 'Marking Scheme', detail: '+2 for each correct answer, No negative marking for incorrect answer' },
+        { title: 'Exam Purpose', detail: 'Determine eligibility for Assistant Professor or both JRF and Assistant Professor in Indian universities and colleges' },
+        { title: 'No. of Test Cities', detail: '181' },
+        { title: 'Official Website & Helpline No.', detail: 'https://ugcnet.nta.nic.in, 0120-6895200' },
+      ],
+      
+      
+      
+      
+      
+      
+    icon: "/images/exam/ugcs.png",
     minQualification: "Graduation",
     benefit: "This is a Test to determine the eligibility of Indian nationals for ‘Assistant Professor’ as well as ‘Junior Research Fellowship and Assistant Professor’ in Indian Universities and Colleges."
 }
