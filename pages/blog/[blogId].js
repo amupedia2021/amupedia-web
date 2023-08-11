1478701138a9e56e6104669ee1396f54ea01a61d
Akanksha Chaurasia <97845909+akanksha984@users.noreply.github.com>
2023-08-11 08:51:11 +0530

    Style All the Blogs (#589)
    * Update blogdata.js
    
    Signed-off-by: Akanksha Chaurasia <97845909+akanksha984@users.noreply.github.com>
    
    * Update [blogId].js
    
    Signed-off-by: Akanksha Chaurasia <97845909+akanksha984@users.noreply.github.com>
    
    * Update BlogDetail.module.css
    
    Signed-off-by: Akanksha Chaurasia <97845909+akanksha984@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Akanksha Chaurasia <97845909+akanksha984@users.noreply.github.com>

--- pages/blog/[blogId].js
+++ pages/blog/[blogId].js
@@ -1,6 +1,5 @@
 import { useRouter } from "next/router";
 import { useState ,useEffect} from "react";
-import Image from "next/image";
 import styles from "@styles/BlogDetail.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUserCircle } from "node_modules/@fortawesome/free-solid-svg-icons/index";
@@ -16,6 +15,7 @@ import {faArrowLeft} from "node_modules/@fortawesome/free-solid-svg-icons/index"
 import BlogCard from "components/Blogs/BlogCard";
 import blogData from "/data/blogdata";
 import Link from "next/link";
+import Footer from "components/common/Footer";
 import ReactMarkdown from "react-markdown";
 import md from "markdown-it";
 export default function BlogId() {
@@ -27,30 +27,30 @@ export default function BlogId() {
   console.log(blog)
   console.log("heelo",blogId);
   
-  
   return (
+    <div className="w-screen">
     <div className={styles.blog}>
       <div className={styles.blogDetail}>
         <div className={styles.blogHeader}>
-          <h2>{blog.title}</h2>
+          <h2 className=" mb-5 leading-tight py-4 fixed top-0 z-30 w-screen bg-white shadow-xl font-semibold font-mono">{blog.title}</h2>
           <div className={styles.blogHeaderInfo}>
             <div className={styles.blogHeaderInfoLeft}>
               <ul>
                 <li>
-                  <a href="/blog">
+                  <a href="/blog" class="text-blue-700 hover:scale-110 hover:text-blue-900 transition duration-200">
                     <FontAwesomeIcon icon={faArrowLeft} />
                     <span> Blogs</span>
                   </a>
                 </li>
-                <li>
+                <li className="-space-x-1">
                   <FontAwesomeIcon icon={faUserCircle} />
                   <span>Test User</span>
                 </li>
-                <li>
+                <li className="-space-x-1">
                   <FontAwesomeIcon icon={faClock} />
                   <span>5 min read</span>
                 </li>
-                <li>
+                <li className="hover:scale-110 -space-x-1">
                   <FontAwesomeIcon icon={faPlayCircle} />
                   <span>Listen</span>
                 </li>
@@ -61,54 +61,53 @@ export default function BlogId() {
                 <li>
                   <Link href="#twitter">
                     <a href="#twitter">
-                      <FontAwesomeIcon icon={faTwitter} />
+                      <FontAwesomeIcon className="hover:text-blue-500 hover:scale-150 transition duration-200" icon={faTwitter}/>
                     </a>
                   </Link>
                 </li>
                 <li>
                   <Link href="#facebook">
                     <a href="#facebook">
-                      <FontAwesomeIcon icon={faFacebook} />
+                      <FontAwesomeIcon className="hover:text-blue-500 hover:scale-150 transition duration-200" icon={faFacebook} />
                     </a>
                   </Link>
                 </li>
                 <li>
                   <Link href="#linkedin">
                     <a href="#linkedin">
-                      <FontAwesomeIcon icon={faLinkedin} />
+                      <FontAwesomeIcon className="hover:text-blue-500 hover:scale-150 transition duration-200" icon={faLinkedin} />
                     </a>
                   </Link>
                 </li>
                 <li>
                   <Link href="#blogLink">
                     <a href="#blogLink">
-                      <FontAwesomeIcon icon={faLink} />
+                      <FontAwesomeIcon className="hover:text-green-500 hover:scale-150 transition duration-200" icon={faLink} />
                     </a>
                   </Link>
                 </li>
               </ul>
               <div className={styles.bookmark_and_more_icon}>
-                <FontAwesomeIcon icon={faBookmark} />
-                <FontAwesomeIcon icon={faEllipsisH} />
+                <FontAwesomeIcon className="hover:text-[#000] cursor-pointer hover:scale-150 transition duration-200" icon={faBookmark} />
+                <FontAwesomeIcon className="hover:text-[#000] cursor-pointer hover:scale-150 transition duration-200" icon={faEllipsisH} />
               </div>
             </div>
           </div>
         </div>
         <div className={styles.blogPicture}>
-          <Image loading="lazy"
+          <img loading="lazy"
             src={blog.image}
-            width="1500"
-            height="1000"
+            width="100%"
+            height="100%"
+            className="inline object-fill w-full"
           />
         </div>
         <div className={styles.blogBody}>
-
-{blog.description}
-   
+            {blog.description} 
         </div>
       </div>
       <div className={styles.otherBlogsCard}>
-        <h3>See other blogs</h3>
+        <h3 className="text-5xl font-serif font-bold underline capitalize tracking-wider">See other blogs</h3>
         <div className={styles.blogCards}>
           {blogData.map((blogItem) => (
             <BlogCard
@@ -117,11 +116,13 @@ export default function BlogId() {
               image={blogItem.image}
               comments={blogItem.comments}
               likes={blogItem.likes}
+              class="hover:shadow-2xl hover:scale-150"
             />
           ))}
         </div>
       </div>
-    </div>
+      <Footer />
+    </div></div>
   );
 }
 
