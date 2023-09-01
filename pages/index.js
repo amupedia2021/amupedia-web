dfbe6706f299632eb42f436eff80154e9d84c2ad
Bhavya <110487270+bhaavvya@users.noreply.github.com>
2023-09-01 13:17:59 +0530

    [Fix]:Update Btech.module.css (#605)
    * Update Btech.module.css
    
    Signed-off-by: Bhavya <110487270+bhaavvya@users.noreply.github.com>
    
    * Update index.js
    
    Signed-off-by: Bhavya <110487270+bhaavvya@users.noreply.github.com>
    
    * Update index.js
    
    Signed-off-by: Bhavya <110487270+bhaavvya@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Bhavya <110487270+bhaavvya@users.noreply.github.com>

--- pages/index.js
+++ pages/index.js
@@ -96,6 +96,7 @@ export default function Home() {
         setMessage('');
         seterror('');
         setsuccess('Contact form sent successfully!!');
+
       }
       setForm(emptyForm);
     }
